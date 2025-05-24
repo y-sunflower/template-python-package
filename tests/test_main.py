ab00f6462c340eea5cdabd02134b2b802c944bb7
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-05-24 20:31:13 +0200

    some improvements and better documentation

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,15 @@
+import pytest
 from package_name import add_digit
 
 
-def test_sum_digit():
-    assert add_digit(1, 1) == 2
-    assert add_digit(10, 1) == 11
-    assert add_digit(10, 10) == 20
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (1, 1, 2),
+        (10, 1, 11),
+        (10, 10, 20),
+        (15, 0, 15),
+    ],
+)
+def test_sum_digit(a, b, expected):
+    assert add_digit(a, b) == expected
