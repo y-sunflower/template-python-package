755d0b852db3d5a2d70e4e957458b3ec98f0ac31
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2025-06-15 09:28:28 +0200

    small fixes

--- package_name/__init__.py
+++ package_name/__init__.py
@@ -1,3 +1,4 @@
 from .main import add_digit
 
+__version__ = "0.1.0"
 __all__ = ["add_digit"]
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,6 @@
 import pytest
+
+import package_name
 from package_name import add_digit
 
 
@@ -13,3 +15,7 @@ from package_name import add_digit
 )
 def test_sum_digit(a, b, expected):
     assert add_digit(a, b) == expected
+
+
+def test_version():
+    assert package_name.__version__ == "0.1.0"
